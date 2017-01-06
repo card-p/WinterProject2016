873cfb5a1a9231b8d1baa5c89c6f3e49172bcd87
b1013217 <b1013217@fun.ac.jp>
2017-01-06 12:17:57 +0000

    wakukimeta

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="ja">
     <head>
       <meta charset="utf-8">
-      <title>未定</title>
+      <title>鬼ごっこ(仮)</title>
 
       <link rel="stylesheet" type="text/css" href="mystyle.css">
       <script type="text/javascript" src="jquery-3.1.0.js"></script>
@@ -10,5 +10,9 @@
     </head>
 
     <body>
+      <h1></h1>
+      <canvas id="game" width="640" height="480">
+        ゲーム画面
+      </canvas>
     </body>
 </html>

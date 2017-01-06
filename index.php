3a3ea32583ebe333746084771171504789f32bf0
card <card.mwpr@gmail.com>
2017-01-06 21:50:39 +0900

    added canvas

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="ja">
     <head>
-      <meta charset="utf-8">
-      <title>鬼ごっこ(仮)</title>
+        <meta charset="utf-8">
+        <title>鬼ごっこ(仮)</title>
 
-      <link rel="stylesheet" type="text/css" href="mystyle.css">
-      <script type="text/javascript" src="jquery-3.1.0.js"></script>
-      <script type="text/javascript" src="script.js"></script>
+        <link rel="stylesheet" type="text/css" href="mystyle.css">
+        <script type="text/javascript" src="jquery-3.1.0.js"></script>
+        <script type="text/javascript" src="script.js"></script>
+        <script src="https://code.createjs.com/createjs-2015.11.26.min.js"></script>
+        <script src="./canvas.js"></script>
     </head>
 
     <body>
       <h1></h1>
-      <canvas id="game" width="640" height="480">
-        ゲーム画面
-      </canvas>
+      <canvas id="game" width="640" height="480"></canvas>
     </body>
 </html>

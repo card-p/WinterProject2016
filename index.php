eb5d0fc618d0d25a3a60b1fd671f7ca86937b6b3
b1013217 <b1013217@fun.ac.jp>
2017-01-06 13:54:36 +0000

    css updated

--- index.php
+++ index.php
@@ -8,11 +8,17 @@
         <script type="text/javascript" src="jquery-3.1.0.js"></script>
         <script type="text/javascript" src="script.js"></script>
         <script src="https://code.createjs.com/createjs-2015.11.26.min.js"></script>
-        <script src="./canvas.js"></script>
+        <script src="canvas.js"></script>
     </head>
 
     <body>
-      <h1></h1>
-      <canvas id="game" width="640" height="480"></canvas>
+      <h1 class="title">鬼ごっこ</h1>
+      <div id="highScore">
+        <div class="title">ハイスコア</div>
+        <div id="scoreTable">
+          <div>1.untiman</div>
+        </div>
+      </div>
+      <canvas id="myCanvas" width="640" height="480"></canvas>
     </body>
 </html>

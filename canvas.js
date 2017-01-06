df8e3b981f653434a5cbc9b889a287eb54b60741
b1013217 <b1013217@fun.ac.jp>
2017-01-06 18:32:02 +0000

    巻き起こる

--- canvas.js
+++ canvas.js
@@ -1,26 +1,44 @@
 
 window.addEventListener("load", init);
 
+var width = 640;
+var height = 480;
+var r = 20;
+//ユーザークラス
+var User = function(posx, posy, stage) {
+    this.enemy = new createjs.Shape();
+    this.enemy.graphics.beginFill("#ffffff");
+    this.enemy.graphics.beginStroke("black");
+    this.enemy.graphics.drawCircle(posx, posy, 20);
+    stage.addChild(this.enemy);
+
+    this.moveRight = function(stage) {
+        this.enemy.x += 1;
+        stage.addChild(this.enemy);
+    }
+}
+//敵クラス
+var Enemy = function(posx, posy, stage) {
+    this.enemy = new createjs.Shape();
+    this.enemy.graphics.beginFill("#a03c44");
+    this.enemy.graphics.beginStroke("black");
+    this.enemy.graphics.drawCircle(posx, posy, 20);
+    stage.addChild(this.enemy);
+}
+
 function init () {
     var stage = new createjs.Stage("myCanvas");
 
-    var enemy = new createjs.Shape();
     var partition = new createjs.Shape();
     //var g = new createjs.Graphics();
-    var width = 640;
-    var height = 480;
-    var r = 20;
     var moveMax = 40;
     var speed = 4;
     var allPartition = [];
 
     // 白い丸(user)
-
+    var user = new User(40, 460, stage);
     // 赤い丸(敵)
-    enemy.graphics.beginFill("#a03c44");
-    enemy.graphics.beginStroke("black");
-    enemy.graphics.drawCircle(40, 100, r);
-    stage.addChild(enemy);
+    var enemy1 =new Enemy(40, 100, stage);
 
     // 仕切りの描画
     partition.graphics.beginStroke("black");
@@ -28,25 +46,25 @@ function init () {
     partition.graphics.lineTo(width, 80);
     stage.addChild(partition);
 
-    allMakePartition(allPartition);
+//    allMakePartition(allPartition);
 
     createjs.Ticker.addEventListener("tick", handleTick);
-          function handleTick() {
+        function handleTick() {
             //  shape.x += 10;
             //  shape.y += 10;
-              stage.update();
-          }
+            user.moveRight(stage);
+            stage.update();
+        }
 }
 
-function allMakePartition(){
-    for(int i = 0; i < 16; i++){
-
-    }
-    for(int j = 0; j < 11; j++){
-        var line = new createjs.Shape();
-        line.graphics.beginStroke("black");
-        line.graphics.moveTo(0, 80);
-        line.graphics.lineTo(width, 80);
-        stage.addChild(line);
-    }
-}
+// function allMakePartition(){
+//     for(int i = 0; i < 16; i++){
+//
+//     }
+//     for(int j = 0; j < 11; j++){
+//         var line = new createjs.Shape();
+//         line.graphics.beginStroke("black");
+//         line.graphics.moveTo(0, 80);
+//         line.graphics.lineTo(width, 80);
+//         stage.addChild(line);
+//     }
